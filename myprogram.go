410970a56a17064381b94baab34245f4e63bc4f3
Matthew Reyes <another-mattr@github.com>
2023-05-23 10:39:06 -0700

    adding a little more logic

--- myprogram.go
+++ myprogram.go
@@ -1,9 +1,13 @@
 package mypackage
 
+import "fmt"
+
 type MyProgram struct {
 	SomeDependency DoThing
 }
 
 func (mp MyProgram) Action(blah string) string {
-	return mp.SomeDependency.Do(blah)
+	word := mp.SomeDependency.Do(blah)
+	phrase := fmt.Sprintf("%s goodbye", word)
+	return phrase
 }
